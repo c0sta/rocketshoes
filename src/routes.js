e63d55def86cfe66db7f91f7f192ec312dcd7492
Gabriel Cm <gabrielcostamoura1@gmail.com>
2020-03-07 21:03:34 -0300

    feat: created basic routes

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { Home, Cart } from './pages';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/cart" component={Cart} />
+    </Switch>
+  );
+}
