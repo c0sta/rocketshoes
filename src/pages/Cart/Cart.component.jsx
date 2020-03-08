0c1dc0ba3a2b8f758caa4cc830384b5fc5f397bc
Gabriel Cm <gabrielcostamoura1@gmail.com>
2020-03-08 09:42:10 -0300

    feat: created Cart table with basic layout

--- src/pages/Cart/Cart.component.jsx
+++ src/pages/Cart/Cart.component.jsx
@@ -1,8 +1,67 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+import { Total, Container, ProductTable } from './Cart.style';
 
-export function Cart(props) {
-  return <div>Carrinho</div>;
+export function Cart() {
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-9-masculino/26/HZM-1276-026/HZM-1276-026_zoom2.jpg?ts=1583630590&ims=326x"
+                alt="tenis"
+              />
+            </td>
+            <td>
+              <strong>Tênis muito nice</strong>
+              <span>R$129,90</span>
+            </td>
+            <td>
+              <div>
+                <button>
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button>
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,90</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1000zão hein pai ta gastador</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
 
 Cart.propTypes = {};
